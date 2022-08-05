e2fdeb798b95e0de5e35c3a361404236c9d98021
Bruno Duarte <duartebruno2208@gmail.com>
2022-08-05 01:08:09 -0300

    styling home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,14 +1,30 @@
+import { Play } from 'phosphor-react'
+import { CountdownContainer, FormContainer, HomeContainer } from './styles'
+
 export function Home() {
   return (
-    <div>
+    <HomeContainer>
       <form action="">
-        <label htmlFor="task">Vou trabalhar em</label>
-        <input id="task" />
+        <FormContainer>
+          <label htmlFor="task">Vou trabalhar em</label>
+          <input id="task" />
 
-        <label htmlFor="minutesAmount">durante</label>
-        <input type="number" id="minutesAmount" />
-        <span>minutos</span>
+          <label htmlFor="minutesAmount">durante</label>
+          <input type="number" id="minutesAmount" />
+          <span>minutos</span>
+        </FormContainer>
+        <CountdownContainer>
+          <span>0</span>
+          <span>0</span>
+          <span>:</span>
+          <span>0</span>
+          <span>0</span>
+        </CountdownContainer>
+        <button type="submit">
+          <Play size={24} />
+          Começar
+        </button>
       </form>
-    </div>
+    </HomeContainer>
   )
 }

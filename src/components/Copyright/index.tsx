fb0401070c534e9ea4044d887deed42fc8ec6da1
Bruno Duarte <duartebruno2208@gmail.com>
2023-06-24 17:26:19 -0300

    copyright

--- src/components/Copyright/index.tsx
+++ src/components/Copyright/index.tsx
@@ -4,7 +4,7 @@ export function Copyright() {
   return (
     <CopyrightContainer>
       <p>Desenvolvido por</p>
-      <a href="https://brunoduarte.dev.br" target="_blank" rel="noreferrer">
+      <a href="https://github.com/bduarte10" target="_blank" rel="noreferrer">
         Bruno Duarte
       </a>
     </CopyrightContainer>
